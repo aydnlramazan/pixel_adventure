e4a6fb46a1981f4182d0fb16da14a66efa6b70cb
aydnlramazan <106530954+aydnlramazan@users.noreply.github.com>
2024-05-10 09:47:01 +0300

    first commit

---
+++ lib/actors/player.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
+
+enum PlayerState { idle, running }
+
+class Player extends SpriteAnimationComponent with HasGameRef<PixelAdventure> {
+  String character;
+  late Map<PlayerState, SpriteAnimation>
+      animations; // animations adlı değişkeni tanımla
+  late PlayerState current; // current adlı değişkeni tanımla
+
+  Player({position, required this.character}) : super(position: position);
+
+  late SpriteAnimation idleAnimation;
+  late SpriteAnimation runningAnimation;
+  final double stepTime = 0.05;
+
+  @override
+  FutureOr<void> onLoad() {
+    _loadAllAnimations();
+    return super.onLoad();
+  }
+
+  void _loadAllAnimations() {
+    idleAnimation = _spriteAnimation('Idle', 11);
+    runningAnimation = _spriteAnimation('Run', 12);
+
+    animations = {
+      PlayerState.idle: idleAnimation,
+      PlayerState.running: runningAnimation
+    };
+
+    current = PlayerState.idle;
+  }
+
+  SpriteAnimation _spriteAnimation(String state, int amount) {
+    return SpriteAnimation.fromFrameData(
+      game.images.fromCache('Main Characters/$character/$state (32x32).png'),
+      SpriteAnimationData.sequenced(
+        amount: amount,
+        stepTime: stepTime,
+        textureSize: Vector2(32, 32),
+      ),
+    );
+  }
+}
